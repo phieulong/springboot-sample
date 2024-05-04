9380de259a3aaae1d2b92a3a1e8e55ede73a26f5
Nguyễn Quang Nhật <phieulong97@gmail.com>
2024-05-04 17:34:10 +0700

    add controller and test

--- src/main/java/phieulong/springbootsample/SpringbootSampleApplication.java
+++ src/main/java/phieulong/springbootsample/SpringbootSampleApplication.java
@@ -2,20 +2,12 @@ package phieulong.springbootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class SpringbootSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSampleApplication.class, args);
-    }
-
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello word";
     }
 }
--- src/test/java/phieulong/springbootsample/SpringbootSampleApplicationTests.java
+++ src/test/java/phieulong/springbootsample/SpringbootSampleApplicationTests.java
@@ -1,13 +1,19 @@
 package phieulong.springbootsample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import phieulong.springbootsample.controller.Home;
 
 @SpringBootTest
 class SpringbootSampleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private Home home;
 
+
+    @Test
+    void test_dependency_injection() {
+        assert home != null;
+    }
 }

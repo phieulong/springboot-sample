5e88c5b46166892b07f04bef4081303a20d48c85
Nguyễn Quang Nhật <phieulong97@gmail.com>
2024-05-04 17:11:45 +0700

    first commit

---
+++ src/test/java/phieulong/springbootsample/SpringbootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package phieulong.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
